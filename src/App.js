0874c4052e8e28295a474576dd83173dc2fb18bd
Hyunah Kim <hyunah2765@gmail.com>
2023-08-12 00:11:59 +0900

    app.js 수정

--- src/App.js
+++ src/App.js
@@ -7,10 +7,7 @@ import CompanyPromotion from "./pages/CompanyPromotion";
 import NotFound from "./pages/NotFound";
 import Login from "./pages/Login";
 import Join from "./pages/Join_p/Join";
-import Join_k from "./pages/Join_p/Join_k";
-import Join_n from "./pages/Join_p/Join_n";
 import Join_tel from "./pages/Join_p/Join_tel";
-import ApplyCheckNon from "./pages/ApplyCheckNon";
 
 const App = () => {
   return (
@@ -29,7 +26,6 @@ const App = () => {
               <Route path="/login/*" element={<Login />} />
               <Route path="/join/*" element={<Join />} />
               <Route path="/join/join_tel" element={<Join_tel />} />
-              {/* <Route path="/join/*" element={<Ji />} /> */}
               <Route path="/*" element={<NotFound />}></Route>
             </Routes>
           </BrowserRouter>
