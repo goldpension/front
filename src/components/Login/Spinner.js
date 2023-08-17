cf26d79b2793934fe713a0abd6fe51638c77c93b
Hyunah Kim <hyunah2765@gmail.com>
2023-08-17 23:56:10 +0900

    스피너 css추가

--- src/components/Login/Spinner.js
+++ src/components/Login/Spinner.js
@@ -3,7 +3,13 @@ import SpinnerLogo from "../../img/SpinnerLogo.gif";
 
 const Spinner = () => {
   return (
-    <div>
+    <div
+      style={{
+        textAlign: "center",
+        paddingTop: "100px",
+        paddingBottom: "20px",
+      }}
+    >
       <h3>잠시만 기다려주세요.</h3>
       <img src={SpinnerLogo} alt="로딩중" width="10%" />
     </div>
