17283f5a517b4b13660d9a7480583a3df30d011a
Hyunah Kim <hyunah2765@gmail.com>
2023-08-18 23:41:56 +0900

    프론트 api최종

--- src/recoil/userState.js
+++ src/recoil/userState.js
@@ -4,9 +4,9 @@ import { atom } from "recoil";
 const userState = atom({
   key: "userState",
   default: {
-    isLoggedIn: true,
-    userName: "이예림",
-    email: "hyuksu0308@naver.com",
+    isLoggedIn: false,
+    userName: null,
+    email: null,
   },
 });
 
