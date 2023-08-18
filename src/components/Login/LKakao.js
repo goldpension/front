0a84334e360cd90c62dc2e7081ccddc0b5a2d17c
yerimi00 <fhddl1019@naver.com>
2023-08-18 23:36:46 +0900

    d

--- src/components/Login/LKakao.js
+++ src/components/Login/LKakao.js
@@ -4,7 +4,7 @@ import styles from "../../css/Login.module.css";
 
 const LKakao = () => {
   const REST_API_KEY = "edf8f58de6f9fb90e53e2dd72452c71f";
-  const REDIRECT_URI = "http://localhost:3000/accounts/kakao/callback/";
+  const REDIRECT_URI = "https://yerimi00.github.io/kakao/callback/";
   const link = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
   const KakaoLoginHandler = () => {
